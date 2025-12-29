4a0920c6ab97915579705963c8a720ad35d98fae
Adams <adamsfuntil@gmail.com>
2025-12-29 14:06:20 -0500

    9.17

---
+++ Ilari/frontend-ilari/src/App.tsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+import type { DiaryEntry } from './types'
+import { getAllDiaries } from './diaryService'
+
+
+const App = () => {
+  const [diaries, setDiaries] = useState<DiaryEntry[]>([])
+
+
+  useEffect(() => {
+    getAllDiaries().then(data => {
+      setDiaries(data)
+    })
+  }, [])
+
+
+
+  console.log('what are diaries---', diaries);
+  return (
+    <div>
+      <h3>Diary Entries</h3>
+      {diaries.map((entry, index) => (
+        <div key={index}>
+          <h4>{entry.date}</h4>
+          <div>
+            <p>visibility: {entry.visibility}</p>
+            <p>weather: {entry.weather}</p>
+          </div>
+        </div>
+      ))}
+
+    </div>
+  )
+}
+
+export default App
---
+++ Ilari/frontend-ilari/src/utils.ts
@@ -0,0 +1,16 @@
+import type { NewDiaryEntry, Weather, Visibility } from "./types";
+import z from "zod";
+
+export const NewEntrySchema = z.object({
+  weather: z.enum(Weather),
+  visibility: z.enum(Visibility),
+  date: z.iso.date(),
+  comment: z.string().optional()
+});
+
+export const toNewDiaryEntry = (object: unknown): NewDiaryEntry => {
+  return NewEntrySchema.parse(object);
+};
+
+
+export default toNewDiaryEntry;
